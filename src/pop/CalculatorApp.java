fe78bca7198c17c19df3d74ffbe49e86073e2497
CGM <mrmouse.guru@gmail.com>
2024-05-23 10:05:21 +0700

    added calling function by pop

--- src/pop/CalculatorApp.java
+++ src/pop/CalculatorApp.java
@@ -3,11 +3,17 @@ package pop;
 public class CalculatorApp {
 
     public static void main(String[] args) {//begin
-        int a = 2;
-        int b = 2;
-        int c = a  * b;
+       int c =  multiply();
         System.out.println("Ket qua: " + c);
     return; 
     }//end
+
+    static int multiply(){//begin
+        int a = 2;
+        int b = 2;
+        int c = a  * b;
+
+        return c;//end
+    }
 
 }
