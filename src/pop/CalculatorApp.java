71e82684f88b7f0c48903ab6139a07bda5941d6a
CGM <mrmouse.guru@gmail.com>
2024-05-23 09:55:13 +0700

    added pop calculating a * b

---
+++ src/pop/CalculatorApp.java
@@ -0,0 +1,13 @@
+package pop;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) {//begin
+        int a = 2;
+        int b = 2;
+        int c = a  * b;
+        System.out.println("Ket qua: " + c);
+    return; 
+    }//end
+
+}
