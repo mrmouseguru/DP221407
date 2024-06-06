3b9464a1f6e39105f2209f979469a22e7171283a
CGM <mrmouse.guru@gmail.com>
2024-06-06 08:22:20 +0700

    initialize Window Swing

---
+++ src/buoi3/CalcualtorWindow.java
@@ -0,0 +1,16 @@
+package buoi3;
+
+import javax.swing.JFrame;
+
+public class CalcualtorWindow extends JFrame{
+
+    
+    CalcualtorWindow(){
+
+        setTitle("Frame Viewer");
+        setSize(400,400);
+        setVisible(true);
+
+    }
+
+}
---
+++ src/buoi3/CalculaltorWindowApp.java
@@ -0,0 +1,9 @@
+package buoi3;
+
+public class CalculaltorWindowApp {
+
+    public static void main(String[] args) {
+        CalcualtorWindow calcualtorWindowRemote = new CalcualtorWindow();
+    }
+
+}
