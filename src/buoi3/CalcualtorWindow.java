a37f6d18cd1250b8c0b8aaaa33149df2a423fa07
CGM <mrmouse.guru@gmail.com>
2024-06-06 08:43:20 +0700

    updated JPanel to Window

--- src/buoi3/CalcualtorWindow.java
+++ src/buoi3/CalcualtorWindow.java
@@ -1,15 +1,35 @@
 package buoi3;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CalcualtorWindow extends JFrame{
-
+    private JLabel jLabelInputRemote1;
+    private JTextField jTextFieldInputRemote1;
+    private JPanel jPanelRemote;
     
     CalcualtorWindow(){
+
+        jPanelRemote = new JPanel();
+
+        jPanelRemote.setBackground(Color.PINK);
+
+        jLabelInputRemote1 = new JLabel("input1");
+        jTextFieldInputRemote1 = new JTextField(10);
+        jPanelRemote.add(jLabelInputRemote1);
+        jPanelRemote.add(jTextFieldInputRemote1);
+
+        add(jPanelRemote);
 
         setTitle("Frame Viewer");
         setSize(400,400);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
     }
 
