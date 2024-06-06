585fb1156322d7b69a29dd144df5035d240a1a5f
CGM <mrmouse.guru@gmail.com>
2024-06-06 08:54:40 +0700

    updated full Window Calculator

--- src/buoi3/CalcualtorWindow.java
+++ src/buoi3/CalcualtorWindow.java
@@ -2,26 +2,24 @@ package buoi3;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CalcualtorWindow extends JFrame{
-    private JLabel jLabelInputRemote1;
-    private JTextField jTextFieldInputRemote1;
+    private JLabel jLabelInputRemote1, 
+    jLabelInputRemote2, jLabelOutputRemote;
+    private JTextField jTextFieldInputRemote1,
+    jTextFieldInputRemote2;
     private JPanel jPanelRemote;
+    private JButton addButtonRemote, subButtonRemote,
+    mulButtonRemote, divButtonRemote;
     
     CalcualtorWindow(){
-
-        jPanelRemote = new JPanel();
-
-        jPanelRemote.setBackground(Color.PINK);
-
-        jLabelInputRemote1 = new JLabel("input1");
-        jTextFieldInputRemote1 = new JTextField(10);
-        jPanelRemote.add(jLabelInputRemote1);
-        jPanelRemote.add(jTextFieldInputRemote1);
+        buildPanel();
+        
 
         add(jPanelRemote);
 
@@ -31,6 +29,28 @@ public class CalcualtorWindow extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+    }
+
+    public void buildPanel() {
+        jPanelRemote = new JPanel();
+
+        //jPanelRemote.setBackground(Color.PINK);
+
+        jLabelInputRemote1 = new JLabel("input1");
+        jTextFieldInputRemote1 = new JTextField(10);
+        jPanelRemote.add(jLabelInputRemote1);
+        jPanelRemote.add(jTextFieldInputRemote1);
+        jLabelInputRemote2 = new JLabel("input2");
+        jTextFieldInputRemote2 = new JTextField(10);
+        jPanelRemote.add(jLabelInputRemote2);
+        jPanelRemote.add(jTextFieldInputRemote2);
+        jLabelOutputRemote = new JLabel("Output");
+        jPanelRemote.add(jLabelOutputRemote);
+        addButtonRemote = new JButton("ADD");
+        subButtonRemote = new JButton("SUB");
+
+        jPanelRemote.add(addButtonRemote);
+        jPanelRemote.add(subButtonRemote);
     }
 
 }
