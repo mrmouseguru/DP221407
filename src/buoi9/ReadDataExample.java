51ff40542b426ddbc91b2c3d73bd02ab0325a35e
CGM <mrmouse.guru@gmail.com>
2024-07-18 10:20:59 +0700

    added Read Data from database Example

--- src/buoi9/ReadDataExample.java
+++ src/buoi9/ReadDataExample.java
@@ -2,7 +2,9 @@ package buoi9;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ReadDataExample {
 
@@ -11,6 +13,8 @@ public class ReadDataExample {
         String url = "jdbc:mysql://localhost:3306/db221407";
         String password = "12345678";
         Connection connection = null;
+        Statement statementRemote = null;
+        ResultSet rsRemote = null;
         //1. KẾT NỐI ĐẾN MYSQL - db221407
         //nạp driver vào chương trình
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -18,10 +22,30 @@ public class ReadDataExample {
         //tạo kết nối đến db221407
         connection = DriverManager.getConnection(url, user, password);
 
-        System.out.println(connection.toString());
+        //System.out.println(connection.toString());
 
         //TRUY VẤN
         String sql = "Select Emp_Id, Emp_No, Emp_Name from Employee";
+
+        //statement
+        statementRemote = connection.createStatement();
+
+        //thực thi câu query
+        rsRemote = statementRemote.executeQuery(sql);
+
+        //in danh sách nhân viên ra màn hình
+        while (rsRemote.next()) {
+            int empId = rsRemote.getInt(1);
+            String empNo = rsRemote.getString(2);
+            String empName = rsRemote.getString("Emp_Name");
+            System.out.println("--------------------");
+            System.out.println("EmpId:" + empId);
+            System.out.println("EmpNo:" + empNo);
+            System.out.println("EmpName:" + empName);
+        }
+
+        connection.close();
+
     }
 
 }
